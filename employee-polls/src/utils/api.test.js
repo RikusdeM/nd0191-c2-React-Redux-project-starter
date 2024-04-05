e3667263f6bb54f7101ab658e9e0775d4d542f7a
Rikus de Milander <rikusdemilander@gmail.com>
2024-04-05 17:56:28 +0200

    Added api tests

---
+++ employee-polls/src/utils/api.test.js
@@ -0,0 +1,95 @@
+import { expect, jest, test, describe, it } from "@jest/globals";
+import {
+  saveQuestion,
+  saveQuestionAnswer,
+  saveQuestionUserUpdate,
+} from "./api";
+
+describe("saveQuestion", () => {
+  it("should return the formatted saved Question if it is passed the needed arguments", async () => {
+    const author = "mtsamis";
+    const question = {
+      optionOneText: "to be",
+      optionTwoText: "or not to be",
+      author: author,
+    };
+    const desiredSavedQuestion = {
+      id: expect.any(String),
+      timestamp: expect.any(Number),
+      author: author,
+      optionOne: {
+        votes: [],
+        text: question.optionOneText,
+      },
+      optionTwo: {
+        votes: [],
+        text: question.optionTwoText,
+      },
+    };
+    const result = await saveQuestion(question);
+
+    expect(result).toEqual(desiredSavedQuestion);
+  });
+
+  it("should fail with a failure message, when the invalid arguments are passed", async () => {
+    const author = "mtsamis";
+    const optionOne = "opt1";
+    const optionTwo = "opt2";
+    const failureMsg =
+      "Please provide optionOneText, optionTwoText, and author";
+
+    await expect(saveQuestion(author)).rejects.toEqual(failureMsg);
+    await expect(saveQuestion(author, optionOne)).rejects.toEqual(failureMsg);
+    await expect(saveQuestion(optionTwo, author)).rejects.toEqual(failureMsg);
+    await expect(saveQuestion(optionOne, optionTwo)).rejects.toEqual(
+      failureMsg
+    );
+  });
+});
+
+describe("saveQuestionAnswer", () => {
+  it("should return true, when the correct argements are passed", async () => {
+    const authedUser = "tylermcginnis";
+    const qid = "am8ehyc8byjqgar0jgpub9";
+    const answer = "optionOne";
+
+    const result = await saveQuestionAnswer({ authedUser, qid, answer });
+
+    expect(result).toEqual(true);
+  });
+
+  it("should fail with a failure message, when the incorrect arguments are passed", async () => {
+    const authedUser = "tylermcginnis";
+    const qid = "am8ehyc8byjqgar0jgpub9";
+    const failureMsg = "Please provide authedUser, qid, and answer";
+
+    await expect(saveQuestionAnswer({ authedUser })).rejects.toEqual(
+      failureMsg
+    );
+    await expect(saveQuestionAnswer({ authedUser, qid })).rejects.toEqual(
+      failureMsg
+    );
+  });
+});
+
+describe("saveQuestionsUserUpdate", () => {
+  it("should return true if the correct arguments are passed", async () => {
+    const authedUser = "sarahedo";
+    const qid = "loxhs1bqm25b708cmbf3g";
+
+    const result = await saveQuestionUserUpdate({ authedUser, qid });
+
+    expect(result).toEqual(true);
+  });
+
+  it("should fail with a failure message, when the wrong arguments are passed", async () => {
+    const authedUser = "sarahedo";
+    const qid = "loxhs1bqm25b708cmbf3g";
+    const failureMsg = "Please provide authedUser and qid";
+
+    await expect(saveQuestionUserUpdate({ authedUser })).rejects.toEqual(
+      failureMsg
+    );
+    await expect(saveQuestionUserUpdate({ qid })).rejects.toEqual(failureMsg);
+  });
+});
