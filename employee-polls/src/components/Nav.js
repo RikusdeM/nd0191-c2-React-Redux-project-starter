4f498cf660da12c118191ead311f4cc5efe97e0a
Rikus de Milander <rikusdemilander@gmail.com>
2024-04-07 22:33:13 +0200

    Updated navbar to set the current navlink active depending on the current URL

--- employee-polls/src/components/Nav.js
+++ employee-polls/src/components/Nav.js
@@ -1,55 +1,83 @@
 import { Link, useNavigate } from "react-router-dom";
 import { connect } from "react-redux";
 import { setAuthedUser } from "../actions/authedUser";
+import { withRouter } from "../utils/helpers";
 
-const Nav = ({ authedUser, users, dispatch }) => {
-  //todo: add active state to navbar, i.e. which tab is currently active
+const Nav = ({ authedUser, users, dispatch, props }) => {
   const navigate = useNavigate();
+
+  const menuItemStates = {
+    HOME: { Name: "Home", URL: "/" },
+    NEW: { Name: "New", URL: "/add" },
+    LEADERBOARD: { Name: "Leaderboard", URL: "/leaderboard" },
+    LOGOUT: { Name: "Logout", URL: "/login" },
+  };
+
+  const currentLocation = props.router.location.pathname;
 
   const handleLogout = (e) => {
     e.preventDefault();
     dispatch(setAuthedUser(null));
-    navigate("/login");
-    // setLinkClicked(true)
-  }
-  
+    navigate(menuItemStates.LOGOUT.URL);
+  };
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <div className="navbar-nav">
-          <Link to="/" className="nav-item nav-link active">
-            Home
-            {/* <span className="sr-only">(current)</span> */}
+          <Link
+            className={
+              currentLocation === menuItemStates.HOME.URL
+                ? "nav-item nav-link active"
+                : "nav-item nav-link"
+            }
+            to={menuItemStates.HOME.URL}
+          >
+            {menuItemStates.HOME.Name}
           </Link>
-          <Link to="/leaderboard" className="nav-item nav-link">
-            Leaderboard
+          <Link
+            className={
+              currentLocation === menuItemStates.LEADERBOARD.URL
+                ? "nav-item nav-link active"
+                : "nav-item nav-link"
+            }
+            to={menuItemStates.LEADERBOARD.URL}
+          >
+            {menuItemStates.LEADERBOARD.Name}
           </Link>
-          <Link to="/add" className="nav-item nav-link">
-            New
+          <Link
+            className={
+              currentLocation === menuItemStates.NEW.URL
+                ? "nav-item nav-link active"
+                : "nav-item nav-link"
+            }
+            to={menuItemStates.NEW.URL}
+          >
+            {menuItemStates.NEW.Name}
           </Link>
         </div>
       </div>
       <div>
         <ul className="navbar-nav mr-auto">
           <li className="li-nav-center">
-            {users[authedUser] ? (        
+            {users[authedUser] ? (
               <div>
                 <span>{authedUser}</span>
                 <img
                   src={`../${users[authedUser].avatarURL}`}
                   style={{ width: 34, height: 34 }}
                   alt={`${users[authedUser].avatarURL}`}
-                />                
+                />
               </div>
             ) : null}
           </li>
           <li>
-            <span className="li-nav-padding" ></span>
+            <span className="li-nav-padding"></span>
           </li>
           <li>
             {authedUser && (
               <Link className="nav-item nav-link" onClick={handleLogout}>
-                Logout
+                {menuItemStates.LOGOUT.Name}
               </Link>
             )}
           </li>
@@ -59,11 +87,12 @@ const Nav = ({ authedUser, users, dispatch }) => {
   );
 };
 
-const mapStateToProps = ({ authedUser, users }) => {
+const mapStateToProps = ({ authedUser, users }, props) => {
   return {
     authedUser,
     users,
+    props,
   };
 };
 
-export default connect(mapStateToProps)(Nav);
+export default withRouter(connect(mapStateToProps)(Nav));
